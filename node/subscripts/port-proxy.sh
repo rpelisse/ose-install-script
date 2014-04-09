1b84df71489d7794deaab57c69bef8ddc6092cb0
Romain PELISSE <belaran@gmail.com>
2014-04-09 18:56:52 +0200

    misc

--- node/subscripts/port-proxy.sh
+++ node/subscripts/port-proxy.sh
@@ -1,7 +1,8 @@
 #/bin/bash
+
 lokkit --port=35531-65535:tcp
-# Run the following command to ensure the proxy service starts on boot:
-chkconfig openshift-port-proxy on
-#Run the following command to start the service immediately:
-service openshift-port-proxy start
-chkconfig openshift-gears on
+# Ensure the proxy service starts on boot:
+chkconfig openshift-port-proxy 'on'
+service openshift-port-proxy 'start'
+
+chkconfig openshift-gears 'on'
